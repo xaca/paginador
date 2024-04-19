db781444344e6b4a361956b1aa3f68e0f069c19d
xaca <and1res@hotmail.com>
2024-04-18 19:00:36 -0500

    Actualizacion de evento de teclas

--- js/main.js
+++ js/main.js
@@ -2,4 +2,14 @@ let teclas;
 
 window.onload = function() {
     teclas = document.getElementsByClassName('tecla');
+    agregarEventos(teclas);
+}
+
+function agregarEventos(teclas){
+    for (let i = 0; i < teclas.length; i++) {
+        teclas[i].addEventListener('click', function(){
+            let valor = this.innerHTML;
+            alert('Presionaste la tecla: ' + valor );
+        });
+    }
 }
