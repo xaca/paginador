caee558d7cf615a75b61c545839c3a0830f9f48c
xaca <and1res@hotmail.com>
2024-04-18 19:19:07 -0500

    Funcionalidad del teclado

--- js/main.js
+++ js/main.js
@@ -1,15 +1,21 @@
 let teclas;
+let pantalla;
 
 window.onload = function() {
+    pantalla = document.getElementById('pantalla');
     teclas = document.getElementsByClassName('tecla');
     agregarEventos(teclas);
+    pantalla.value = '';
 }
 
 function agregarEventos(teclas){
     for (let i = 0; i < teclas.length; i++) {
         teclas[i].addEventListener('click', function(){
             let valor = this.innerHTML;
-            alert('Presionaste la tecla: ' + valor );
+            if(pantalla.value.length<10)
+            {
+                pantalla.value += valor;
+            }
         });
     }
 }
