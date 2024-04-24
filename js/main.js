3c42410096ab9304e59977ed7cef03a8b6aa3129
xaca <and1res@hotmail.com>
2024-04-23 19:18:30 -0500

    Invocacion dependiendo del contexto

--- js/main.js
+++ js/main.js
@@ -1,21 +1,57 @@
-let teclas;
-let pantalla;
+let persona;
 
 window.onload = function() {
-    pantalla = document.getElementById('pantalla');
-    teclas = document.getElementsByClassName('tecla');
-    agregarEventos(teclas);
-    pantalla.value = '';
+    persona = {};//Creación de un objeto vacío
+    /* Propiedades */
+    persona.nombre = "Juan"; //Asignación de una propiedad al objeto
+    persona.apellido = "Pérez"; //Asignación de una propiedad al objeto
+    persona.edad = 30; //Asignación de una propiedad al objeto
+    persona.direccion = "Calle 123"; //Asignación de una propiedad al objeto
+    persona.telefono = "123456789"; //Asignación de una propiedad al objeto
+    persona.email = "admin@upb.edu.co"; //Asignación de una propiedad al objeto
+
+    /* Métodos */
+    
+    persona.saludar = saludar; //Asignación de un método al objeto
+    persona.hablar = hablar; //Asignación de un método al objeto
+    //alert(persona);
+    persona.saludar(); //Llamado a un método del objeto
+    persona.hablar("Estoy hablando en español"); //Llamado a un método del objeto
+
+    //Eliminación de una propiedad
+    //delete persona.telefono;
+
+    //Eliminación de una propiedad
+    //persona.email = undefined;
+
+    //Recorrido de las propiedades del objeto
+    /*for (let propiedad in persona) {
+        console.log(propiedad + ": " + persona[propiedad]);
+    }*/
+
+    saludar();
 }
 
-function agregarEventos(teclas){
-    for (let i = 0; i < teclas.length; i++) {
-        teclas[i].addEventListener('click', function(){
-            let valor = this.innerHTML;
-            if(pantalla.value.length<10)
-            {
-                pantalla.value += valor;
-            }
-        });
+
+/*
+
+        Tabla de verdad
+                        AND       OR       NOT
+    p         q        p && q   p || q      !p
+    true      true     true      true     false
+    true      false    false     true     false
+    false     true     false     true     true
+    false     false    false     false    true
+
+*/
+
+function saludar() {
+    if(this.nombre && this.apellido)
+    {
+        alert("Hola, soy " + this.nombre + " " + this.apellido);
     }
+}
+
+function hablar(mensaje){
+    alert(mensaje);
 }
