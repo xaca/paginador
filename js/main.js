e35d28c5109fe9fde89b627eb66a9545d365f00f
xaca <and1res@hotmail.com>
2024-04-24 20:23:03 -0500

    Ejercio inicial del paginador

--- js/main.js
+++ js/main.js
@@ -1,74 +1,20 @@
-let teclas, btn_borrar;
-let pantalla;
+let datos = [];
 
-const teclado = document.getElementById('teclado');
-const numeros = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+/* Terminar de llenar el array con nuevos productos, completar al menos 30 productos */
 
 window.onload = function() {
-    pantalla = document.getElementById('pantalla');
-    teclas = document.getElementsByClassName('item');
-    btn_borrar = document.querySelector('.borrar');
-    pantalla.value = '';
-    
-    btn_borrar.addEventListener('click', function(){ pantalla.value = ''; });
-    
-    // Generar el teclado aleatorio al cargar la página
-    generarTeclado();
-    agregarEventos(teclas);
+    datos.push({
+        nombre: "Iphone 15", 
+        precio: 1000, 
+        cantidad: 5,
+        descripcion: "El mejor celular del mercado", 
+        imagen: "https://icons.iconarchive.com/icons/designcontest/ecommerce-business/128/iphone-icon.png"
+    });
+    datos.push({
+        nombre: "Ipad mini", 
+        precio: 700, 
+        cantidad: 12,
+        descripcion: "La mejor tablet del mercado", 
+        imagen: "https://icons.iconarchive.com/icons/john-freeborn/ipad/128/iPad-flip-case-standing-icon.png"
+    });
 }
-
-function agregarEventos(teclas){
-    let tecla;
-    for (let i = 0; i < teclas.length; i++) 
-    {
-        tecla = teclas[i];
-        tecla.setAttribute('valor', numeros[i]);
-        tecla.addEventListener('click', function(event){
-            let valor = event.target.getAttribute('valor');
-
-            if(pantalla.value.length<10)
-            {
-                pantalla.value += valor;
-            }
-        });
-        tecla.addEventListener('mouseover', (event) => {
-            //event.target.textContent = '*';
-            ponerAsteriscos();
-        });
-
-        tecla.addEventListener('mouseout', (event) => {
-            /*let temp = event.target;
-            temp.textContent = temp.getAttribute('valor');*/
-            mostrarNumeros();
-        });
-    }
-}
-
-function mostrarNumeros(){
-    for (let i = 0; i < teclas.length; i++) {
-        teclas[i].innerHTML = numeros[i];
-    }
-}
-
-function ponerAsteriscos(){
-    for (let i = 0; i < teclas.length; i++) {
-        teclas[i].innerHTML = '*';
-    }
-}
-
-// Función para barajar un array unsort
-function barajarArray(array) {
-    for (let i = array.length - 1; i > 0; i--) {
-        const j = Math.floor(Math.random() * (i + 1));
-        [array[i], array[j]] = [array[j], array[i]];
-    }
-}
-
-function generarTeclado() {
-    
-    barajarArray(numeros);
-    for (let i = 0; i < teclas.length; i++) {
-        const tecla = teclas[i];
-        tecla.textContent = numeros[i];
-    }
-  }
