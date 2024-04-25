ac28c21f6f17ef818f0993f13fd24a18989a02a0
xaca <and1res@hotmail.com>
2024-04-24 19:09:14 -0500

    Actualizacion de teclado aleatorio usando gemini

--- js/main.js
+++ js/main.js
@@ -1,11 +1,21 @@
 let teclas;
 let pantalla;
 
+const teclado = document.getElementById('teclado');
+const numeros = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+
 window.onload = function() {
     pantalla = document.getElementById('pantalla');
     teclas = document.getElementsByClassName('tecla');
     agregarEventos(teclas);
     pantalla.value = '';
+
+    /*for (let i = 0; i < teclas.length; i++) {
+        teclas[i].innerHTML = '*';
+    } */
+
+    // Generar el teclado aleatorio al cargar la página
+    generarTeclado();
 }
 
 function agregarEventos(teclas){
@@ -19,3 +29,20 @@ function agregarEventos(teclas){
         });
     }
 }
+
+// Función para barajar un array unsort
+function barajarArray(array) {
+    for (let i = array.length - 1; i > 0; i--) {
+        const j = Math.floor(Math.random() * (i + 1));
+        [array[i], array[j]] = [array[j], array[i]];
+    }
+}
+
+function generarTeclado() {
+    
+    barajarArray(numeros);
+    for (let i = 0; i < teclas.length; i++) {
+        const tecla = teclas[i];
+        tecla.textContent = numeros[i];
+    }
+  }
