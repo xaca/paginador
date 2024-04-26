e70f69f1363d723555bac8ef116420fb55dfd9d6
xaca <and1res@hotmail.com>
2024-04-25 19:01:25 -0500

    Primera version de prueba de la UI del paginador

--- js/main.js
+++ js/main.js
@@ -1,8 +1,35 @@
 let datos = [];
-
-/* Terminar de llenar el array con nuevos productos, completar al menos 30 productos */
+let gallery;
 
 window.onload = function() {
+   createFakeData();
+   gallery = document.getElementsByClassName("gallery");
+}
+
+function renderCards(){
+    let cards = "";
+    for(let i = 0; i < datos.length; i++){
+        cards += renderCard(datos[i]);
+    }
+}
+
+function renderCard(obj){
+    //Template literal cuando se usan las comillas francesas ``
+    let card = `<section class="card">
+    <div>
+        <h3>${obj.nombre}</h3>
+        <span>${obj.precio}</span>
+        <span>${obj.cantidad}</span>
+        <p>${obj.descripcion}</p>
+        <a href="#" class="btn">Ver más</a>
+    </div>
+    <div>
+        <img src="${obj.imagen}" alt="">
+    </div>`;
+    return card;
+}
+
+function createFakeData(){
     datos.push({
         nombre: "Iphone 15", 
         precio: 1000, 
@@ -17,4 +44,56 @@ window.onload = function() {
         descripcion: "La mejor tablet del mercado", 
         imagen: "https://icons.iconarchive.com/icons/john-freeborn/ipad/128/iPad-flip-case-standing-icon.png"
     });
+    datos.push({
+        nombre: "Bicicleta Electrica", 
+        precio: 3000, 
+        cantidad: 15,
+        descripcion: "Bicicleta electrica con bateria de litio", 
+        imagen: "https://icons.iconarchive.com/icons/iconshock/global-warming/128/bicycle-icon.png"
+    });
+    datos.push({
+        nombre: "Reloj inteligente", 
+        precio: 1200, 
+        cantidad: 5,
+        descripcion: "Reloj inteligente con pantalla tactil", 
+        imagen: "https://icons.iconarchive.com/icons/microsoft/fluentui-emoji-flat/128/American-Football-Flat-icon.png"
+    });
+    datos.push({
+        nombre: "Televisor smart", 
+        precio: 1000, 
+        cantidad: 5,
+        descripcion: "Televisor smart de 50 pulgadas", 
+        imagen: "https://icons.iconarchive.com/icons/microsoft/fluentui-emoji-flat/128/Accordion-Flat-icon.png"
+    });
+    datos.push({
+        nombre: "Nevera", 
+        precio: 700, 
+        cantidad: 12,
+        descripcion: "Nevera de 2 puertas", 
+        imagen: "https://icons.iconarchive.com/icons/heylove/vintage/128/vintage-fridge-green-icon.png"
+    });
+    datos.push({
+        nombre: "Lavadora", 
+        precio: 1000, 
+        cantidad: 5,
+        descripcion: " Lavaora de ropa de 10 kilos", 
+        imagen: "https://icons.iconarchive.com/icons/iconshock/electrical-appliances/128/washing-machine-icon.png"
+    });
+    
+    datos.push({
+        nombre: "Camiseta", 
+        precio: 1000, 
+        cantidad: 5,
+        descripcion: "Camiseta de algodon", 
+        imagen: "https://icons.iconarchive.com/icons/michael/nike/128/Nike-Shirt-1-icon.png"
+    });
+
+    datos.push({
+        nombre: "Cargador de portatil", 
+        precio: 1000, 
+        cantidad: 5,
+        descripcion: "Cargador de portatil universal", 
+        imagen: "https://icons.iconarchive.com/icons/microsoft/fluentui-emoji-flat/128/Alien-Flat-icon.png"
+    });
 }
+
